42d33ce57e5ce77a3de566d1f9535b163689bfe0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-06-19 06:57:23 +0000

    Commit

--- src/design/patterns/creational/builder/Coke.java
+++ src/design/patterns/creational/builder/Coke.java
@@ -1,5 +1,15 @@
 package design.patterns.creational.builder;
 
-public class Coke {
+public class Coke extends Coldrink {
+
+	@Override
+	public String name() {
+		return "Coke";
+	}
+
+	@Override
+	public double price() {
+		return 55.00;
+	}
 
 }
--- src/design/patterns/creational/builder/Pepsi.java
+++ src/design/patterns/creational/builder/Pepsi.java
@@ -1,5 +1,15 @@
 package design.patterns.creational.builder;
 
-public class Pepsi {
+public class Pepsi extends Coldrink {
+
+	@Override
+	public String name() {
+		return "Pepsi";
+	}
+
+	@Override
+	public double price() {
+		return 60.00;
+	}
 
 }
