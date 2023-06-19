99757b0a21e12f9146046bd8a6d61942724699f4
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-06-19 07:06:34 +0000

    Update Meal.java

--- src/design/patterns/creational/builder/Meal.java
+++ src/design/patterns/creational/builder/Meal.java
@@ -1,5 +1,30 @@
 package design.patterns.creational.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
+
+	private List<Item> items = new ArrayList<>();
+
+	public void addItem(Item item) {
+		items.add(item);
+	}
+
+	public void showItems() {
+		items.forEach((i) -> {
+			System.out.println(i.name());
+			System.out.println(i.packing().pack());
+			System.out.println(i.price());
+		});
+	}
+
+	public double getCost() {
+		double cost = 0.0;
+//		items.forEach(i -> cost += i.price());
+		for (Item i : items)
+			cost += i.price();
+		return cost;
+	}
 
 }
