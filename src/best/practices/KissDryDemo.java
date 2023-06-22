c30f531fb21f8308ff7940c9aec4ecc6d23de07b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-06-22 07:32:29 +0000

    Update KissDryDemo.java

--- src/best/practices/KissDryDemo.java
+++ src/best/practices/KissDryDemo.java
@@ -6,8 +6,33 @@ public class KissDryDemo {
 	String berthType = ""; // lower, middle, upper, side-lower, side-upper
 
 	String getBerthType(int berthNumber) {
-		// logic 
+		// better logic ?
 		return null;
 	}
 
+	String getBerthType2(int berthNumber) {
+		if (berthNumber < 1 || berthNumber > 72)
+			return "Invalid input!";
+		String[] berthTypes = { "lower", "middle", "upper", "lower", "middle", "upper", "side-lower", "side-upper" };
+		return berthTypes[berthNumber % 8];
+	}
+
+	String getBerthType3(int berthNumber) {
+		String berthType = "";
+		if (berthNumber >= 1 && berthNumber <= 72) {
+			if (berthNumber % 8 == 1 || berthNumber % 8 == 4)
+				berthType = "lower";
+			else if (berthNumber % 8 == 2 || berthNumber % 8 == 5)
+				berthType = "middle";
+			else if (berthNumber % 8 == 4 || berthNumber % 8 == 6)
+				berthType = "upper";
+			else if (berthNumber % 8 == 7)
+				berthType = "side-lower";
+			else
+				berthType = "side-upper";
+		} else {
+			return "Invalid input!"; // ex 
+		}
+		return berthType;
+	}
 }
